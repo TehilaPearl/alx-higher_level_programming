cf429eb9569f6350754afdc062d6dcff7c8049df
Olufunke <olufunkeadejoke4@gmail.com>
2022-05-31 21:33:28 -0700

    task 9 print last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def print_last_digit(number):
+
+    if number >= 0:
+        result = number % 10
     if number < 0:
-        number = -number
-    last_digit = number % 10
-    print("{}".format(last_digit), end="")
-    return 
+        result = (number % -10) * -1
+    print("{:d}".format(result), end='')
+    return result
