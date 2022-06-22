dd0312a6a314b3e0acc7cfd27af5a34d0ec092c5
Olufunke <olufunkeadejoke4@gmail.com>
2022-06-22 16:51:39 -0700

    added 2 safe print file

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-
     try:
-        res = a / float(b)
+        c = a/b
     except ZeroDivisionError:
-        res = None
+        c = None
     finally:
-        print("Inside result: {}".format(res))
-        return (res)
-    
+        print("Inside result: {}".format(c))
+    return (c)
