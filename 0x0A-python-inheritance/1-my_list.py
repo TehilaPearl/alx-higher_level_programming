9a099146a5ba40f679e46e592700252d3a827d4c
Olufunke <olufunkeadejoke4@gmail.com>
2022-07-06 12:27:30 -0700

    added 1 my list file

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"A module that defines a class named MyList"
+
+
+class MyList(list):
+    """A class named MyList
+    Attributes:
+    attr1(print_sorted): prints sorted list
+    """
+    def print_sorted(self):
+        """Prints instance"""
+        print(sorted(self))
+        
